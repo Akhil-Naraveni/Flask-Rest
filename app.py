aa71d9f188714b5a376020979f0f08dfd444eae1
Akhil Naraveni <naraveniakhil@gmail.com>
2022-09-21 11:34:32 +0530

    a

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+
 app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
+app.secret_key = 'akhil'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
